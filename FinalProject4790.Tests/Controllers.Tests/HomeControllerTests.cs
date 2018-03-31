1ba8a8bb7529fdc139da901e33ffea78c9785d24
Ethan Brown <ethanbrown3@mail.weber.edu>
2018-03-30 21:35:58 -0600

    Added tests to HomeControllerTests

--- FinalProject4790.Tests/Controllers.Tests/HomeControllerTests.cs
+++ FinalProject4790.Tests/Controllers.Tests/HomeControllerTests.cs
@@ -3,28 +3,55 @@ using NUnit.Framework;
 using FinalProject4790.Controllers;
 using FinalProject4790.Models.DomainServices;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace Tests
 {
     public class HomeControllerTests
     {
+
+        private MockProductRepository mockProducts;
+        private MockSellerRepository mockSellers;
+        private HomeController controller;
+
         [SetUp]
         public void Setup()
         {
-            
+            // Arrange
+            mockSellers = new MockSellerRepository();
+            mockProducts = new MockProductRepository();
+            controller = new HomeController(mockSellers, mockProducts);
         }
 
         [Test]
         public void HomeControllerIndexReturnsViewResult()
         {
-            // Arrange
-            var mockSellers = new MockSellerRepository();
-            var mockProducts = new MockProductRepository();
-            var controller = new HomeController(mockSellers, mockProducts);
             // Act
-            var result = controller.Index();
+            var result = controller.Index() as ViewResult;
+            var model = result.Model;
             // Assert
             Assert.IsInstanceOf<ViewResult>(result);
+            Assert.IsNotNull(model);
+        }
+
+        [Test]
+        public void HomeControllerProductListReturnsViewResult()
+        {
+            var result = controller.ProductList(0, "SellerNameTest") as ViewResult;
+            var model = result.Model;
+            
+            Assert.IsInstanceOf<ViewResult>(result);
+            Assert.IsNotNull(model);
+        }
+
+        [Test]
+        public void HomeControllerProductDetailReturnsViewResult()
+        {
+            var result = controller.ProductDetail(0) as ViewResult;
+            var model = result.Model;
+            
+            Assert.IsInstanceOf<ViewResult>(result);
+            Assert.IsNotNull(model);
         }
     }
 }
