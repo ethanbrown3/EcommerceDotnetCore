5817f9c153872ad4f4dfb21b612521ddee7e89c1
Ethan Brown <ethanbrown3@mail.weber.edu>
2018-03-31 21:11:55 -0600

    added useurls to program

--- FinalProject4790/Program.cs
+++ FinalProject4790/Program.cs
@@ -19,6 +19,7 @@ namespace FinalProject4790
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://*:5000")
                 .UseStartup<Startup>()
                 .Build();
     }
