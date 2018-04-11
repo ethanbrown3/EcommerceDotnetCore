b6792751eb3a4ff9f3a47b6a796b466667831df8
Ethan Brown <ethanbrown3@mail.weber.edu>
2018-04-10 21:03:53 -0600

    Added dropdown admin menu

---
+++ FinalProject4790/Views/Components/AdminMenu.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinalProject4790.Views.Components
+{
+    public class AdminMenu : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var menuItems = new List<AdminMenuItem> { 
+                new AdminMenuItem()
+                {
+                    DisplayValue = "User management",
+                    ActionValue = "UserManagement"
+
+                },
+                new AdminMenuItem()
+                {
+                    DisplayValue = "Role management",
+                    ActionValue = "RoleManagement"
+                }};
+
+            return View(menuItems);
+        }
+    }
+
+    public class AdminMenuItem
+    {
+        public string DisplayValue { get; set; }
+        public string ActionValue { get; set; }
+    }
+}
---
+++ FinalProject4790/Views/ViewModels/AddUserViewModel.cs
@@ -0,0 +1,22 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FinalProject4790.Views.ViewModels
+{
+    public class AddUserViewModel
+    {
+        [Required]
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+    }
+}
