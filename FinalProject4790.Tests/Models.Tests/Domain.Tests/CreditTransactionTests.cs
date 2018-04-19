12ce4fb0b0b410287ae05a62d6f270c26fa7d3e8
Ethan Brown <ethanbrown3@mail.weber.edu>
2018-04-18 22:50:00 -0600

    CreditTransaction model tests

---
+++ FinalProject4790.Tests/Models.Tests/Domain.Tests/CreditTransactionTests.cs
@@ -0,0 +1,88 @@
+using System;
+using NUnit.Framework;
+using FinalProject4790.Controllers;
+using FinalProject4790.Models.Domain;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace FinalProject4790.Tests.Models.Domain.Tests
+{
+    public class CreditTransactionTests
+    {
+        private CreditTransaction creditTransaction;
+        [SetUp]
+        public void Setup()
+        {
+            // Arrange
+            creditTransaction = new CreditTransaction();
+        }
+
+        [Test]
+        public void CreditTransaction_SetGetId()
+        {
+            creditTransaction.CreditTransactionId = 100;
+
+            Assert.AreEqual(creditTransaction.CreditTransactionId, 100);
+        }
+
+        [Test]
+        public void CreditTransaction_SetGetStripeChargeId()
+        {
+            creditTransaction.StripeChargeId = "TestID";
+
+            Assert.AreEqual(creditTransaction.StripeChargeId, "TestID");
+        }
+
+        [Test]
+        public void CreditTransaction_SetGetCreatedDate()
+        {
+            var testDateTime = DateTime.Now;
+            creditTransaction.Created = testDateTime;
+
+            Assert.AreEqual(creditTransaction.Created, testDateTime);
+        }
+
+        [Test]
+        public void CreditTransaction_SetGetAmountInCents()
+        {
+            creditTransaction.AmountInCents = 100;
+
+            Assert.AreEqual(creditTransaction.AmountInCents, 100);
+        }
+
+        [Test]
+        public void CreditTransaction_SetGetPaid()
+        {
+            creditTransaction.Paid = true;
+
+            Assert.True(creditTransaction.Paid);
+        }
+
+        [Test]
+        public void CreditTransaction_SetGetOrder()
+        {
+            var order = new Order();
+            order.OrderId = 1;
+
+            creditTransaction.Order = order;
+            creditTransaction.OrderId = order.OrderId;
+
+            Assert.AreEqual(creditTransaction.Order, order);
+            Assert.AreEqual(creditTransaction.OrderId, order.OrderId);
+        }   
+
+        [Test]
+        public void CreditTransaction_SetGetSeller()
+        {
+            var seller = new Seller();
+            seller.SellerId = 1;
+
+            creditTransaction.Seller = seller;
+            creditTransaction.SellerId = seller.SellerId;
+
+            Assert.AreEqual(creditTransaction.Seller, seller);
+            Assert.AreEqual(creditTransaction.SellerId, seller.SellerId);
+        }
+    }
+}
